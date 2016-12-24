14fe178195764b56bcb0ab00939126227ea3c07e
myztajay <myztajay@hotmail.com>
2016-12-24 19:07:06 +0000

    Added Create to compare controller

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -1,7 +1,37 @@
 class ComparesController < ApplicationController
   def show
+    @compare = Compare.find(params[:id])
   end
+  
+  def new
+    @compare = Compare.new
+  end
+  
+  def create
+    @compare = Compare.new(compare_parameters)
+    @compare.user_id = current_user.id
+    @compare.save
+    
+    if @compare.save
+    redirect_to compare_path(@compare)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  def compare_parameters
+    params.require(:compare).permit(:headline, :img1_url, :img2_url, :user_id)
+  end
+  
 
-  def index
-  end
 end
