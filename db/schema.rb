a64350615dce8475ba4b85edd4948c23130d4deb
myztajay <myztajay@hotmail.com>
2016-12-24 17:13:55 +0000

    Added credit to user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223213657) do
+ActiveRecord::Schema.define(version: 20161224165907) do
 
   create_table "compares", force: :cascade do |t|
     t.string   "img1_url"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20161223213657) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.integer  "credit", :null => false, :default => 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
