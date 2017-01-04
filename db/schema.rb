6d7694ec13cf352ac3cd306623a3d4a3044bb3df
myztajay <myztajay@hotmail.com>
2017-01-04 15:36:52 +0000

    Fixed materialize asking for fonts, Added name + image to user as experiment for oauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229192533) do
+ActiveRecord::Schema.define(version: 20170103202930) do
 
   create_table "compares", force: :cascade do |t|
     t.string   "img1_url"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20161229192533) do
     t.integer  "credit"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
