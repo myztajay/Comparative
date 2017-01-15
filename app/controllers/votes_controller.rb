725736bd6c1714d1dcdbd4c5a8c9fa728e7f3bd2
myztajay <myztajay@hotmail.com>
2017-01-15 23:06:21 +0000

    Fixed  bug where app crashed if user wasnt signed in and tried to vote or view compares

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,14 +2,15 @@ class VotesController < ApplicationController
   
   def create
     
+ if user_signed_in?   
     @vote = Vote.new()
     @vote.user_id = current_user.id
     @vote.compare_id = params[:compare_id].to_i
     @vote.choice = params[:choice]
     @compare = Compare.find(params[:compare_id])
     
-    if !@vote.save #offloaded the work of checking to ACTIVE RECORD if save fail its not unique.
-      redirect_to root_path, :flash => {:error => "You cannot vote twice, Lets keep it fair."}
+  if !@vote.save #offloaded the work of checking to ACTIVE RECORD if save fail its not unique.
+    redirect_to root_path, :flash => {:error => "You cannot vote twice, Lets keep it fair."}
     else 
       if params[:choice] === "this"
        
@@ -24,6 +25,10 @@ class VotesController < ApplicationController
       end
     end
   end
+  
+else
+  redirect_to  new_user_session_path
+end
   
 end
 
