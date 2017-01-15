b9682a6fed9f88f7ae2511eb301baebeedb2999c
myztajay <myztajay@hotmail.com>
2017-01-15 23:22:52 +0000

    Really fixed the last bug where unsigned in users crash app if they try to vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,15 +2,15 @@ class VotesController < ApplicationController
   
   def create
     
- if user_signed_in?   
+ if user_signed_in? 
     @vote = Vote.new()
     @vote.user_id = current_user.id
     @vote.compare_id = params[:compare_id].to_i
     @vote.choice = params[:choice]
     @compare = Compare.find(params[:compare_id])
     
-  if !@vote.save #offloaded the work of checking to ACTIVE RECORD if save fail its not unique.
-    redirect_to root_path, :flash => {:error => "You cannot vote twice, Lets keep it fair."}
+    if !@vote.save #offloaded the work of checking to ACTIVE RECORD if save fail its not unique.
+      redirect_to root_path, :flash => {:error => "You cannot vote twice, Lets keep it fair."}
     else 
       if params[:choice] === "this"
        
@@ -23,11 +23,10 @@ class VotesController < ApplicationController
         @compare.save
         redirect_to root_path
       end
-    end
-  end
-  
-else
-  redirect_to  new_user_session_path
+     end
+   else
+redirect_to  new_user_session_path
+ end
 end
   
 end
